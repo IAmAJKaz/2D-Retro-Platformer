206c1c034bd79ff93e6f04a709579d454359d52f
AJ <kazazian.aj@maranathastudents.org>
2021-03-04 09:53:54 -0800

    Vid 15
    Death animations for enemies

--- Assets/Scripts/EnemyHP.cs
+++ Assets/Scripts/EnemyHP.cs
@@ -7,15 +7,30 @@ public class EnemyHP : MonoBehaviour {
     public int enemyHP;
     private int currentHP;
 
+    private Animator theAnim;
+    private bool isDead;
+
+    private Collider2D parentCol;
+    private Collider2D hurtboxCol;
 
     void Start() {
         currentHP = enemyHP;
+
+        theAnim = transform.parent.GetComponent<Animator>();
+
+        parentCol = transform.parent.GetComponent<Collider2D>();
+        hurtboxCol = GetComponent<Collider2D>();
     }
 
 
     void Update() {
         if (currentHP <= 0) {
-            Destroy(transform.parent.gameObject);
+            isDead = true;
+            theAnim.SetBool("Dead", isDead);
+            parentCol.enabled = false;
+            hurtboxCol.enabled = false;
+            StartCoroutine("KillSwitch");
+            //Destroy(transform.parent.gameObject);
         }
     }
 
@@ -23,5 +38,8 @@ public class EnemyHP : MonoBehaviour {
         currentHP -= damage;
     }
 
-
+    IEnumerator KillSwitch() {
+        yield return new WaitForSeconds(2f);
+        Destroy(transform.parent.gameObject);
+    }
 }
